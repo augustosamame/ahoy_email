4beca4a3c43de75077ffb11f85dc052c25f119ee
Augusto Samamé <augustosamame@gmail.com>
2018-08-31 05:38:10 -0500

    refactored secret key base

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -3,19 +3,7 @@ require "rails/engine"
 module AhoyEmail
   class Engine < ::Rails::Engine
     initializer "ahoy_email" do |app|
-      AhoyEmail.secret_token ||= begin
-        creds =
-          if app.respond_to?(:credentials) && app.credentials.secret_key_base
-            app.credentials
-          elsif app.respond_to?(:secrets)
-            app.secrets
-          else
-            app.config
-          end
-
-        creds.respond_to?(:secret_key_base) ? creds.secret_key_base : creds.secret_token
-      end
-
+      AhoyEmail.secret_token ||= ENV["SECRET_KEY_BASE"]
       AhoyEmail.belongs_to = {optional: true} if Rails::VERSION::MAJOR >= 5
     end
   end
--- lib/ahoy_email/version.rb
+++ lib/ahoy_email/version.rb
@@ -1,3 +1,3 @@
 module AhoyEmail
-  VERSION = "0.5.4"
+  VERSION = "0.5.5"
 end
